6314b8622f3da98f00070c7f4a01636d6a0a50e9
ice00 <ice@host-7826-93-122.popwifi.it>
2015-11-26 21:16:56 +0100

    Fix new api call

--- includes/api/external.php
+++ includes/api/external.php
@@ -1,9 +1,12 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if($_SERVER['REQUEST_METHOD'] != "POST") die("Invalid use of API");
 
 require_once('../../../../../wp-load.php');
 
-if (get_option('newstatpress_externalapi')!='checked') die("API not activated");;
+if (get_option('newstatpress_externalapi')!='checked') die("API not activated");
 
 // get the parameter from URL
 $var = $_REQUEST["VAR"];
@@ -31,14 +34,15 @@ switch ($var) {
     break;
   case 'overview':
     // use a switch to avoid invalid parameters are passed
-    switch ($par):
+    switch ($par) {
       case 'main':
       case 'dashboard':
       case 'null':
-        nsp_ApiOverview($par);
+        nsp_ApiOverview();
         break;
       default:
         die("Invalid PAR value for overview API.");        
+    }
     break;  
   default: 
     die("Not recognized API.");
@@ -50,6 +54,9 @@ switch ($var) {
  * Return the current version of newstatpress as json encoded
  */
 function nsp_ApiVersion() {
+  global $_NEWSTATPRESS;
+  global $var;
+
   echo json_encode(
     array(
       $var => $_NEWSTATPRESS['version']
@@ -61,11 +68,14 @@ function nsp_ApiVersion() {
  * API: overview
  *
  * Return the overview according to the passed parameters as json encoded
- *
- * @param par the API paramenter
  */
-function nsp_ApiOverview($par) {
-  $result='';
+function nsp_ApiOverview() {
+  global $var;
+  global $par;
+  global $wpdb;
+  $table_name = nsp_TABLENAME;
+
+  $result=array();
 
   $thisyear = gmdate('Y', current_time('timestamp'));
 
@@ -74,27 +84,32 @@ function nsp_ApiOverview($par) {
 
     switch($row) {
       case 'visitors' :
+        $row2='DISTINCT ip';
         $sql_QueryTotal="SELECT count($row2) AS $row FROM $table_name WHERE feed='' AND spider=''";
-       break;
+        break;
       case 'visitors_feeds' :
+        $row2='DISTINCT ip';
         $sql_QueryTotal="SELECT count($row2) AS $row FROM $table_name WHERE feed<>'' AND spider='' AND agent<>''";
         break;
       case 'pageview' :
+        $row2='date';
         $sql_QueryTotal="SELECT count($row2) AS $row FROM $table_name WHERE feed='' AND spider=''";
         break;
       case 'spiders' :
+        $row2='date';
         $sql_QueryTotal="SELECT count($row2) AS $row FROM $table_name WHERE feed='' AND spider<>''";
         break;
       case 'feeds' :
+        $row2='date';
         $sql_QueryTotal="SELECT count($row2) AS $row FROM $table_name WHERE feed<>'' AND spider=''";
         break;
     }
 
     $qry_total = $wpdb->get_row($sql_QueryTotal);
     $qry_tyear = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$thisyear%'"); 
-
-    $result+="$row._visitors_total => $qry_total
-              $row._visitors_tyear => $qry_tyear"; 
+ 
+    $result[$row.'_total'] = $qry_total->$row;
+    $result[$row.'_tyear'] = $qry_tyear->$row;
   }
 
   // gives the complete output
