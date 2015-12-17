7526c550e2a4a7163075af99861418dcc2fe9650
ice00 <ice@host-7826-93-122.popwifi.it>
2015-12-17 20:52:47 +0100

    Add overview/dasboard as external API

--- includes/api/external.php
+++ includes/api/external.php
@@ -1,69 +1,144 @@
 <?php
-#header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *'); 
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if($_SERVER['REQUEST_METHOD'] != "POST") die("Invalid use of API");
-
 require_once('../../../../../wp-load.php');
 
-if (get_option('newstatpress_externalapi')!='checked') die("API not activated");
+$var;
+$key; 
+$par; 
+$typ;
+$resultJ;
+$resultH;
 
-// get the parameter from URL
-$var = $_REQUEST["VAR"];
-$key = $_REQUEST["KEY"];  # key readed is md5(date('m-d-y H i')+'Key')
-$par = $_REQUEST["PAR"];  # can be empty
+$resultJ=array();
+$resultH="";
 
-if( !preg_match("/^[a-zA-Z0-9 ]*$/",$key) ) die("Invalid key");
-
-if ($var == null && $key == null) die("API needs parameters");
-
-global $wpdb;
-$table_name = $wpdb->prefix . "statpress";
-
-// read key from wordpress option
-$api_key=get_option('newstatpress_apikey');
-$api_key=md5(gmdate('m-d-y H i').$api_key);
+body();
 
 
-// test if can use API
-if ($key != $api_key)  die("Not authorized API access.");
+/**
+ * body function
+ */
+function body() {
+  global $_NEWSTATPRESS;
+  global $var;
+  global $key; 
+  global $par; 
+  global $typ; 
+  global $wpdb;
+  global $resultJ;
+  global $resultH;
 
-switch ($var) {
-  case 'version':
-    nsp_ApiVersion();
-    break;
-  case 'overview':
-    // use a switch to avoid invalid parameters are passed
-    switch ($par) {
-      case 'main':
-      case 'dashboard':
-      case 'null':
-        nsp_ApiOverview();
-        break;
-      default:
-        die("Invalid PAR value for overview API.");        
-    }
-    break;  
-  default: 
-    die("Not recognized API.");
+  header('HTTP/1.0 200 Ok');
+
+  if($_SERVER['REQUEST_METHOD'] != "POST") {
+    header('HTTP/1.0 403 Forbidden'); 
+    die("Invalid use of API");
+    return;
+  }
+
+  if (get_option('newstatpress_externalapi')!='checked') {
+    header('HTTP/1.0 403 Forbidden'); 
+    die("API not activated");
+    return;
+  }
+
+  // read key from wordpress option
+  $api_key=get_option('newstatpress_apikey');
+  $api_key=md5(gmdate('m-d-y H i').$api_key);
+
+  // get the parameter from URL
+  $var = $_REQUEST["VAR"];
+  $key = $_REQUEST["KEY"];  # key readed is md5(date('m-d-y H i').'Key')
+  $par = $_REQUEST["PAR"];  # can be empty
+  $typ = $_REQUEST["TYP"];  # can be empty
+
+  if ($typ == null) $typ="JSON";
+
+  if ($typ != "JSON" && $typ != "HTML") {
+    header('HTTP/1.0 403 Forbidden'); 
+    die("Return type not available");
+    return;
+  }
+
+  if( !preg_match("/^[a-zA-Z0-9 ]*$/",$key) )  {
+    header('HTTP/1.0 403 Forbidden');
+    die("Invalid key");
+    return;
+  }
+
+  if ($var == null && $key == null) {
+    header('HTTP/1.0 403 Forbidden');
+    die("API needs parameters");
+    return;
+  }
+
+  // test if can use API
+  if ($key != $api_key) {
+    header('HTTP/1.0 403 Forbidden');
+    die("Not authorized API access");
+    return;
+  }
+
+  switch ($var) {
+    case 'version':
+      nsp_ApiVersion();
+      break;
+    case 'overview':
+      // use a switch to avoid invalid parameters are passed
+      switch ($par) {
+        case 'main':
+        case 'dashboard':
+        case 'null':
+          nsp_ApiOverview();
+          break;
+        default:
+          header('HTTP/1.0 403 Forbidden');
+          die("Invalid PAR value for overview API.");    
+          return;   
+      }
+      break;  
+    default: 
+      header('HTTP/1.0 403 Forbidden'); 
+      die("Not recognized API.");
+      return;
+  }
+
+  if ($typ == 'JSON') {
+    // gives the complete output according to $resultJ
+    echo json_encode(
+      $resultJ
+    ); 
+  }
+
+  if ($typ == 'HTML') {
+    // gives the complete output according to $resultH
+    echo $resultH; 
+  }
 }
 
 /**
  * API: version
  *
- * Return the current version of newstatpress as json encoded
+ * Return the current version of newstatpress as json/html
  */
 function nsp_ApiVersion() {
   global $_NEWSTATPRESS;
   global $var;
+  global $typ;
+  global $resultJ;
+  global $resultH;
 
-  echo json_encode(
-    array(
-      $var => $_NEWSTATPRESS['version']
-    )
-  );  
+  $resultJ=array(
+    $var => $_NEWSTATPRESS['version']
+  );
+
+  if ($typ=="JSON") return;  // avoid to calculte HTML if not necessary
+
+  $resultH="<div>".$resultJ[$var]."</div>";  
 }
 
 /**
@@ -75,13 +150,18 @@ function nsp_ApiOverview() {
   global $var;
   global $par;
   global $wpdb;
-  $table_name = nsp_TABLENAME;
+  global $resultJ;
+  global $resultH;
 
-  $result=array();
+  $table_name = nsp_TABLENAME;
 
   $thisyear = gmdate('Y', current_time('timestamp'));
 
+  $resultJ['since']=nsp_ExpandVarsInsideCode('%since%');  // export
+  $resultJ['lastmonth']=nsp_Lastmonth();                  // export
+
   $overview_rows=array('visitors','visitors_feeds','pageview','feeds','spiders');
+
   foreach ($overview_rows as $row) {
 
     switch($row) {
@@ -109,17 +189,118 @@ function nsp_ApiOverview() {
 
     $qry_total = $wpdb->get_row($sql_QueryTotal);
     $qry_tyear = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$thisyear%'"); 
- 
-    $result[$row.'_total'] = $qry_total->$row;
-    $result[$row.'_tyear'] = $qry_tyear->$row;
+
+    $resultJ[$row.'_total'] = $qry_total->$row;  // export
+    $resultJ[$row.'_tyear'] = $qry_tyear->$row;  // export
+
+    if (get_option($nsp_option_vars['calculation']['name'])=='sum') {
+
+      // alternative calculation by mouth: sum of unique visitors of each day
+      $tot=0;
+      $t = getdate(current_time('timestamp'));
+      $year = $t['year'];
+      $month = sprintf('%02d', $t['mon']);
+      $day= $t['mday'];
+      $totlm=0;
+
+      for($k=$t['mon'];$k>0;$k--)
+      {
+        //current month
+
+      }
+      for($i=0;$i<$day;$i++)
+      {
+        $qry_daylmonth = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$lastmonth$i%'");
+        $qry_day=$wpdb->get_row($sql_QueryTotal. " AND date LIKE '$year$month$i%'");
+        $tot+=$qry_day->$row;
+        $totlm+=$qry_daylmonth->$row;
+
+      }
+      // echo $totlm." ,";
+      $qry_tmonth->$row=$tot;
+      $qry_lmonth->$row=$totlm;
+
+    }
+    else { // classic
+      $qry_tmonth = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$thismonth%'");
+      $qry_lmonth = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$lastmonth%'");
+    }
+
+    $resultJ[$row.'_tmonth'] = $qry_tmonth->$row;  // export
+    $resultJ[$row.'_lmonth'] = $qry_lmonth->$row;  // export
+
+    $qry_y = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$yesterday'");
+    $qry_t = $wpdb->get_row($sql_QueryTotal. " AND date LIKE '$today'");
+
+    $resultJ[$row.'_qry_y'] = $qry_y->$row;  // export
+    $resultJ[$row.'_qry_t'] = $qry_t->$row;  // export
+
+    $calculated_result=nsp_CalculateVariation($qry_tmonth,$qry_lmonth,$row);
+    $resultJ[$row.'_cresult'] = $calculated_result;  // export
   }
 
-  // gives the complete output
-  echo json_encode(
-    array(
-      $result
-    )
-  );  
+  if ($typ=="JSON") return;  // avoid to calculte HTML if not necessary
+
+  // output a HTML representation of the collected data
+
+  $overview_table='';
+
+  $since = $resultJ['since'];
+  $lastmonth = $resultJ['lastmonth'];
+
+  //$thisyear = gmdate('Y', current_time('timestamp'));
+  $thismonth = gmdate('Ym', current_time('timestamp'));
+  $yesterday = gmdate('Ymd', current_time('timestamp')-86400);
+  $today = gmdate('Ymd', current_time('timestamp'));
+  $tlm[0]=substr($lastmonth,0,4); $tlm[1]=substr($lastmonth,4,2);
+
+  $thisyearHeader = gmdate('Y', current_time('timestamp'));
+  $lastmonthHeader = gmdate('M, Y',gmmktime(0,0,0,$tlm[1],1,$tlm[0]));
+  $thismonthHeader = gmdate('M, Y', current_time('timestamp'));
+  $yesterdayHeader = gmdate('d M', current_time('timestamp')-86400);
+  $todayHeader = gmdate('d M', current_time('timestamp'));
+
+  // dashboard
+  $overview_table.="<table class='widefat center nsp'>
+                      <thead>
+                      <tr class='sup dashboard'>
+                      <th></th>
+                          <th scope='col'>". __('M-1','newstatpress'). "</th>
+                          <th scope='col' colspan='2'>". __('M','newstatpress'). "</th>
+                          <th scope='col'>". __('Y','newstatpress'). "</th>
+                          <th scope='col'>". __('T','newstatpress'). "</th>
+                      </tr>
+                      <tr class='inf dashboard'>
+                      <th></th>
+                          <th><span>$lastmonthHeader</span></th>
+                          <th colspan='2'><span > $thismonthHeader </span></th>
+                          <th><span>$yesterdayHeader</span></th>
+                          <th><span>$todayHeader</span></th>
+                      </tr></thead>
+                      <tbody class='overview-list'>";
+
+  foreach ($overview_rows as $row) {
+    $result=$resultJ[$row.'_cresult'];
+
+    // build full current row
+    $overview_table.="<tr><td class='row_title $row'>$row_title</td>";
+    if ($print=='main')
+      $overview_table.="<td class='colc'>".$resultJ[$row.'_total']."</td>\n";
+    if ($print=='main')
+      $overview_table.="<td class='colc'>".$resultJ[$row.'_tyear']."</td>\n";
+    $overview_table.="<td class='colc'>".$resultJ[$row.'_lmonth']."</td>\n";
+    $overview_table.="<td class='colr'>".$resultJ[$row.'_tmonth'].$result[0] ."</td>\n";
+    if ($print=='main')
+      $overview_table.="<td class='colr'>".$result[1]." ".$result[2]."</td>\n";
+    $overview_table.="<td class='colc'>".$resultJ[$row.'_qry_y']."</td>\n";
+    $overview_table.="<td class='colc'>".$resultJ[$row.'_qry_t']."</td>\n";
+    $overview_table.="</tr>";
+  }
+
+  $overview_table.="</tr></table>";
+
+  $resultH=$overview_table;  
+
 }
 
 
