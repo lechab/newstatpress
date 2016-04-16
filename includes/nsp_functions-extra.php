756c3d3eb4f351ff0c6b172120534245e57027b3
chab <chabdev@free.fr>
2016-04-16 13:30:26 +0200

    undefined offset
    Signed-off-by: chab <chabdev@free.fr>

--- includes/nsp_functions-extra.php
+++ includes/nsp_functions-extra.php
@@ -113,7 +113,8 @@ function nsp_GetDaysInstalled() {
  * @return the extracted url
  *************************************/
 function nsp_ExtractFeedFromUrl($url) {
-  list($null,$q)=explode("?",$url);
+  //list($null,$q)=explode("?",$url); old def before PHP7, to delete if no pb.
+  list($null,$q)=array_pad(explode("?",$url,2), 2, null);
 
   if (strpos($q, "&")!== false)
     list($res,$null)=explode("&",$q);
